5757b51cdc7ff722251f848e6ec8b31ec4ee31d9
Rafael Malave <rafomalave@gmail.com>
2015-08-16 13:15:55 -0700

    Add pages and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
   root "posts#index"
+
+  get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
